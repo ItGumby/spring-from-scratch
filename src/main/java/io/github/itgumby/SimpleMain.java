d507d97c981d6512bfe79679519decd18e207233
Brian Street <brian.street@kaseware.com>
2025-10-04 20:50:03 -0600

    enable Spring annotations

--- src/main/java/io/github/itgumby/SimpleMain.java
+++ src/main/java/io/github/itgumby/SimpleMain.java
@@ -1,7 +1,12 @@
 package io.github.itgumby;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
 public class SimpleMain {
     public static void main(String[] args) {
-
+        System.out.println("starting @SpringBootApplication SimpleMain");
+        SpringApplication.run(SimpleMain.class, args);
     }
 }
