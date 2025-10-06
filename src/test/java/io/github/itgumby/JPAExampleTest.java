20cc360c254b7247ab3b4a07f11d7d960df735b4
Brian Street <brian.street@kaseware.com>
2025-10-06 13:37:22 -0600

    broken unit test: H2 db doesnt have table

--- src/test/java/io/github/itgumby/JPAExampleTest.java
+++ src/test/java/io/github/itgumby/JPAExampleTest.java
@@ -1,11 +1,78 @@
 package io.github.itgumby;
 
+import java.net.URI;
+import java.net.URL;
+import java.time.LocalDate;
+
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 
 public class JPAExampleTest {
+
+    private SessionFactory sessionFactory;
+
+    @BeforeEach
+    protected void setUp() {
+        // based on https://docs.jboss.org/hibernate/orm/7.1/quickstart/html_single/
+        // A SessionFactory is set up once for an application!
+        final StandardServiceRegistry registry =
+                new StandardServiceRegistryBuilder().build();
+        try {
+            sessionFactory = new MetadataSources(registry)
+                            .addAnnotatedClass(Meet.class)
+                            .buildMetadata()
+                            .buildSessionFactory();
+        }
+        catch (Exception e) {
+            // The registry would be destroyed by the SessionFactory, but we
+            // had trouble building the SessionFactory so destroy it manually.
+            System.out.println("\nEXCEPTION: " + e.getClass().getSimpleName() + "[" + e.getMessage() + "]\n");
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+
+    @AfterEach
+    protected void teardown() throws Exception {
+        if (null != sessionFactory) {
+            sessionFactory.close();
+        }
+    }
+
+    @Test
+    void saveMyFirstEntity() {
+        URL url = null;
+        // try {
+        //     url = new URI("https://co.milesplit.com/meets/659366-cheyenne-mountain-stampede-pre-state-2025/results/1195884/formatted")
+        //             .toURL();
+        // } catch (Exception ignore) {
+        //     //no-op
+        // }
+        Meet meet = new Meet();
+        meet.setYear(2025);
+        meet.setName("Test Meet");
+        meet.setDate(LocalDate.now());
+        meet.setLocation("Anytown, USA");
+        meet.setTemperature(70);
+        meet.setWeather("sunny");
+        meet.setUrl(url);
+
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+
+            session.persist(meet);
+
+            session.getTransaction().commit();
+        } // auto-close session
+    }
 
     @Disabled
     @Test
